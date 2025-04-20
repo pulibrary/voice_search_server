5372e97e483b5d550fbc247e89ea0c4d087b0c6a
Jane Sandberg <js7389@princeton.edu>
2025-04-19 17:11:47 -0700

    Opus decodinggit add .

--- src/audio.rs
+++ src/audio.rs
@@ -1,12 +1,22 @@
-use anyhow::Result;
-use symphonia::{core::{audio::{AudioBufferRef, Signal}, codecs::{CodecParameters, CodecType, Decoder, DecoderOptions, CODEC_TYPE_VORBIS}, formats::{FormatOptions, FormatReader}, io::{MediaSource, MediaSourceStream, MediaSourceStreamOptions}}, default::{codecs::VorbisDecoder, formats::MkvReader}};
-use symphonia::core::conv::FromSample;
+use std::any;
+
+use anyhow::{Result, anyhow};
+use opus::{Channels, Decoder};
+use symphonia::{
+    core::{
+        codecs::CODEC_TYPE_OPUS,
+        formats::FormatReader,
+        io::{MediaSource, MediaSourceStream},
+    },
+    default::formats::MkvReader,
+};
 
 // This module is responsible for decoding Webm audio into
 // a vector of PCM samples (see https://en.wikipedia.org/wiki/Pulse-code_modulation)
 //
 // Webm uses a Matroska (aka MKV) format and a Vorbis or OPUS encoding.
-// This code supports only the Vorbis encoding.
+// This code supports only the OPUS encoding, since that is the encoding
+// that Firefox and Chromium use.
 //
 // Each sample is expressed in a 32 bit float
 // The sample rate is taken from the provided audio
@@ -15,50 +25,61 @@ use symphonia::core::conv::FromSample;
 //   See: https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder/MediaRecorder#audiobitspersecond
 // The bitrate is 32 * the sample rate
 
-fn conv<T>(samples: &mut Vec<f32>, data: std::borrow::Cow<symphonia::core::audio::AudioBuffer<T>>)
-where
-    T: symphonia::core::sample::Sample,
-    f32: symphonia::core::conv::FromSample<T>,
-{
-    samples.extend(data.chan(0).iter().map(|v| f32::from_sample(*v)))
+struct Track {
+    sample_rate: u32,
+    channels: Channels,
+    id: u32,
+    reader: MkvReader,
 }
 
-fn pcm_decode(original: impl MediaSource + 'static) -> Result<(Vec<f32>, u32)> {
-    let stream = MediaSourceStream::new(Box::new(original), Default::default());
-    let mut reader = MkvReader::try_new(stream, &Default::default()).unwrap();
-    let first_track = reader.tracks().iter().find(|t| t.codec_params.codec == CODEC_TYPE_VORBIS).expect("No Vorbis tracks in this file!");
-    let track_id = first_track.id;
-    let sample_rate = first_track.codec_params.sample_rate.unwrap_or(0);
-    let dec_opts: DecoderOptions = Default::default();
-    // Create a decoder for the track.
-    let mut decoder = symphonia::default::get_codecs()
-        .make(&first_track.codec_params, &dec_opts)
-        .expect("unsupported codec");
+pub fn pcm_decode(original: impl MediaSource + 'static) -> Result<(Vec<f32>, u32)> {
+    let mut track = demux(original)?;
+    // Sample rate must be 8000, 12000, 16000, 24000, or 48000.
+    let mut decoder = Decoder::new(48_000, track.channels).unwrap();
+
     let mut pcm_data = Vec::new();
-    while let Ok(packet) = reader.next_packet() {
+    while let Ok(packet) = track.reader.next_packet() {
         // Consume any new metadata that has been read since the last packet.
-        while !reader.metadata().is_latest() {
-            reader.metadata().pop();
+        while !track.reader.metadata().is_latest() {
+            track.reader.metadata().pop();
         }
 
         // If the packet does not belong to the selected track, skip over it.
-        if packet.track_id() != track_id {
+        if packet.track_id() != track.id {
             continue;
         }
-        match decoder.decode(&packet)? {
-            AudioBufferRef::F32(buf) => pcm_data.extend(buf.chan(0)),
-            AudioBufferRef::U8(data) => conv(&mut pcm_data, data),
-            AudioBufferRef::U16(data) => conv(&mut pcm_data, data),
-            AudioBufferRef::U24(data) => conv(&mut pcm_data, data),
-            AudioBufferRef::U32(data) => conv(&mut pcm_data, data),
-            AudioBufferRef::S8(data) => conv(&mut pcm_data, data),
-            AudioBufferRef::S16(data) => conv(&mut pcm_data, data),
-            AudioBufferRef::S24(data) => conv(&mut pcm_data, data),
-            AudioBufferRef::S32(data) => conv(&mut pcm_data, data),
-            AudioBufferRef::F64(data) => conv(&mut pcm_data, data),
-        }
+        let num_samples = decoder.get_nb_samples(packet.buf())?;
+        let mut decoded = vec![0.0; num_samples * 2];
+        let res = decoder.decode_float(packet.buf(), &mut decoded, false);
+        pcm_data.append(&mut decoded);
     }
-    Ok((pcm_data, sample_rate))
+
+    Ok((pcm_data, track.sample_rate))
+}
+
+fn demux(original: impl MediaSource + 'static) -> Result<Track> {
+    let stream = MediaSourceStream::new(Box::new(original), Default::default());
+    let reader = MkvReader::try_new(stream, &Default::default()).unwrap();
+    let first_track_option = reader
+        .tracks()
+        .iter()
+        .find(|t| t.codec_params.codec == CODEC_TYPE_OPUS);
+    let first_track = match first_track_option {
+        Some(track) => track,
+        None => return Err(anyhow!("No Opus tracks in this file!")),
+    };
+    let sample_rate = first_track.codec_params.sample_rate.unwrap_or(0);
+    let channel_count = first_track.codec_params.channels.iter().count();
+    Ok(Track {
+        sample_rate,
+        id: first_track.id,
+        channels: if channel_count == 1 {
+            Channels::Mono
+        } else {
+            Channels::Stereo
+        },
+        reader,
+    })
 }
 
 #[cfg(test)]
@@ -69,9 +90,15 @@ mod tests {
 
     #[test]
     fn it_can_pcm_decode() {
-        let file = File::open("./test_data/audacity-vorbis.webm").unwrap();
+        let file = File::open("./test_data/opus.webm").unwrap();
         let (samples, rate) = pcm_decode(file).unwrap();
         assert!(samples.len() > 100_000);
-        assert_eq!(rate, 44100);
+        assert_eq!(rate, 48_000);
+    }
+
+    #[test]
+    fn it_errors_on_vorbis_encoding() {
+        let file = File::open("./test_data/vorbis.webm").unwrap();
+        assert!(pcm_decode(file).is_err());
     }
 }
